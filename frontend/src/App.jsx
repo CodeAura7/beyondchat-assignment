9b5000f63e9cf22e6db1f80dff04a077363ad493
CodeAura7 <shivlad24@gmail.com>
2025-12-29 17:10:14 +0530

    setup react frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main className="container">
+        <h1>BeyondChats</h1>
+        <p>Article platform frontend initialized.</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
