5be7d38504ed79769456a2d916fb8d7bde507a6a
CodeAura7 <shivlad24@gmail.com>
2025-12-29 16:32:12 +0530

    setup express server with health check

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const articleRoutes = require('./routes/article.routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK', message: 'Server is healthy' });
+});
+
+app.use('/api/articles', articleRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+module.exports = app;
