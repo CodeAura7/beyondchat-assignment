08c1e9fc76c89a198d17b323e7ecf03257ccff66
CodeAura7 <shivlad24@gmail.com>
2025-12-30 14:59:18 +0530

    fix route order: scrape before :id

--- backend/src/routes/article.routes.js
+++ backend/src/routes/article.routes.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 const articleController = require('../controllers/article.controller');
 
+// static routes FIRST
+router.post('/scrape', articleController.scrapeArticles);
 
+// collection routes
 router.post('/', articleController.createArticle);
 router.get('/', articleController.getAllArticles);
+
+// dynamic routes LAST
 router.get('/:id', articleController.getArticleById);
 router.put('/:id', articleController.updateArticle);
 router.delete('/:id', articleController.deleteArticle);
-router.post('/scrape', articleController.scrapeArticles);
-
 
 module.exports = router;
